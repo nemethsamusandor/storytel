5ec6f2e3a8a7a168242d6d1bbbd1c107bd98442d
nsa <extern.sandor.nemeth@porscheinformatik.com>
2021-04-21 10:22:29 +0200

    Initial upload   - Project frame

---
+++ MessageBoardServer/src/main/java/se/storytel/messageboard/MessageBoardApplication.java
@@ -0,0 +1,15 @@
+package se.storytel.messageboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBoardApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MessageBoardApplication.class, args);
+    }
+
+}
---
+++ MessageBoardServer/src/test/java/se/storytel/messageboard/MessageBoardApplicationTests.java
@@ -0,0 +1,15 @@
+package se.storytel.messageboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBoardApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
